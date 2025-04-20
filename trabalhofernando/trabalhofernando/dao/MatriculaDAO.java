8b33393f1b1704012a60b5aadb26c6cf00bcd037
Nathany <103070255+Thanyzinha@users.noreply.github.com>
2025-04-20 20:30:56 -0300

    Add files via upload

---
+++ trabalhofernando/trabalhofernando/dao/MatriculaDAO.java
@@ -0,0 +1,19 @@
+package dao;
+
+import java.util.List;
+
+import model.Matricula;
+
+public class MatriculaDAO {
+
+    public void salvar(Matricula matricula) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'salvar'");
+    }
+
+    public List<Matricula> listar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listar'");
+    }
+    
+}
